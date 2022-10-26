ec661c5c33a16921ef6bf5d5a43e1a3cd020bd31
AswathiMohan23 <achu999here@gmail.com>
2022-10-26 08:39:04 +0530

    [REMOVED] : unwanted comments and spaces

--- src/com/java/PlayerMethods.java
+++ src/com/java/PlayerMethods.java
@@ -196,27 +196,4 @@ public class PlayerMethods {
         }
         return "Nobody Won";
     }
-   /* public String winningConditions() {
-        System.out.println("1");
-        if ((((gameBoard[0][0] + gameBoard[0][1] + gameBoard[0][2])) == "XXX") || (((gameBoard[1][0] + gameBoard[1][1] + gameBoard[1][2])) == "XXX")
-                || (((gameBoard[2][0] + gameBoard[2][1] + gameBoard[2][2])) == "XXX") || (((gameBoard[0][0] + gameBoard[1][1] + gameBoard[2][2])) == "XXX") ||
-                (((gameBoard[0][2] + gameBoard[1][1] + gameBoard[2][0])) == "XXX") || (((gameBoard[0][0] + gameBoard[1][0] + gameBoard[2][0])) == "XXX")
-                || (((gameBoard[0][1] + gameBoard[1][1] + gameBoard[2][1])) == "XXX") || (((gameBoard[0][2] + gameBoard[1][2] + gameBoard[2][2])) == "XXX")) {
-
-            System.out.println("You won the game");
-
-            System.out.println("2");
-            return "won";
-        } else if ((((gameBoard[0][0] + gameBoard[0][1] + gameBoard[0][2])) == "OOO") || (((gameBoard[1][0] + gameBoard[1][1] + gameBoard[1][2])) == "OOO")
-                || (((gameBoard[2][0] + gameBoard[2][1] + gameBoard[2][2])) == "OOO") || (((gameBoard[0][0] + gameBoard[1][1] + gameBoard[2][2])) == "OOO") ||
-                (((gameBoard[0][2] + gameBoard[1][1] + gameBoard[2][0])) == "OOO") || (((gameBoard[0][0] + gameBoard[1][0] + gameBoard[2][0])) == "OOO")
-                || (((gameBoard[0][1] + gameBoard[1][1] + gameBoard[2][1])) == "OOO") || (((gameBoard[0][2] + gameBoard[1][2] + gameBoard[2][2])) == "OOO")) {
-            System.out.println("You won the game");
-            System.out.println("3");
-            return "won";
-        }
-
-        return null;
-    }*/
-
-}
+   }
