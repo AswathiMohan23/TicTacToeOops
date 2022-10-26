93badeea2ad98e60f7c3c12da84827e9ab3de518
AswathiMohan23 <achu999here@gmail.com>
2022-10-26 09:31:31 +0530

    [REMOVED] : unwanted comments and spaces

--- src/com/java/TicTacToe.java
+++ src/com/java/TicTacToe.java
@@ -4,9 +4,8 @@ public class TicTacToe {
     public static void main(String[] args){
         TicTacToeDetails details=new TicTacToeDetails();
         PlayerMethods playerMethods=new PlayerMethods();
-        System.out.println(" Welcome to Tic-Tac-Toe\n=========================\n"+details.toString());
+        System.out.println(" Welcome to Tic-Tac-Toe\n=========================\n");
         playerMethods.board();
         playerMethods.chooseLetter();
-       // playerMethods.game();
     }
 }
--- src/com/java/TicTacToeDetails.java
+++ src/com/java/TicTacToeDetails.java
@@ -1,37 +1,9 @@
 package com.java;
 
 public class TicTacToeDetails {
-   // char [][] gameBoard = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int[][] board = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    //String[][] gameBoard=new String[4][4];
-    //private String[][] gameBoard=new String[][]{{"00","01","02"}, {"10","11","12"}, {"20","21","22"}};
-   // private String[][] gameBoard = new String[3][3];
-    //private String[][] gameBoard=new String[][]{{"a","b","c"}, {"d","e","f"}, {"g","h","i"}};
-    private String[][] gameBoard=new String[][]{{"1","2","3"}, {"4","5","6"}, {"7","8","9"}};
-
-
+   private String[][] gameBoard=new String[][]{{"1","2","3"}, {"4","5","6"}, {"7","8","9"}};
     private String PLAYER1;
     private String PLAYER2;
-
-
-    /* public void setGameBoard(int[][] gameBoard,int position,String choice) {
-         while(position!=0){
-         for(int i=0;i<position;i++)
-         {
-             System.out.println();
-             for(int j=0;j<position;j++)
-             {
-                 board[position][position]= Integer.parseInt(choice);
-                 System.out.print(board[position][position]+"\t");
-
-
-             }
-         }
-         System.out.println("\n");
-     }
-         }
- */
-
 
     public void setBoard() {
         for (int i = 0; i < 3; i++) {
@@ -42,9 +14,7 @@ public class TicTacToeDetails {
             }
         }
         System.out.println("\n");
-
     }
-
 
     public String getPLAYER1() {
         return PLAYER1;
