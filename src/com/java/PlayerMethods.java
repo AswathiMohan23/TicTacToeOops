1201d0cd0d94a7bd43d87e03a632c1f0c70dd291
AswathiMohan23 <achu999here@gmail.com>
2022-11-21 14:26:19 +0530

    [REFACTORED] : the code to include hints to win the game

--- src/com/java/PlayerMethods.java
+++ src/com/java/PlayerMethods.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class PlayerMethods {
     Scanner sc = new Scanner(System.in);
     TicTacToeDetails details = new TicTacToeDetails();
-    //public String[][] gameBoard = new String[][]{{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}};
+    static String playerChoice1;
+    static String playerChoice2;
     public String[][] gameBoard = details.getGameBoard();
 
     public void chooseLetter() {
@@ -42,24 +43,32 @@ public class PlayerMethods {
             details.setPLAYER1(choice.toUpperCase());
             System.out.println("\nplayer1 has chosen =====> " + details.getPLAYER1() + " ....so " + otherPlayer + " can take =====> O");
             String otherChoice = "o";
+            playerChoice1="X";
+            playerChoice2="O";
             game(choice, "player1", otherPlayer, otherChoice);
 
         } else if ((player.equals("player1")) && ((choice.equals("o")) || (choice.equals("O")))) {
             details.setPLAYER1(choice.toUpperCase());
             System.out.println("\nplayer1 has chosen =====>" + details.getPLAYER1() + " ....so " + otherPlayer + " can take =====> X");
             String otherChoice = "x";
+            playerChoice1="O";
+            playerChoice2="X";
             game(choice, "player1", otherPlayer, otherChoice);
 
         } else if ((player.equals("player2")) && ((choice.equals("x")) || (choice.equals("X")))) {
             details.setPLAYER2(choice.toUpperCase());
             System.out.println("\nplayer2 has chosen =====> " + details.getPLAYER2() + " ....so " + otherPlayer + " can take =====> O");
             String otherChoice = "o";
+            playerChoice2="X";
+            playerChoice1="O";
             game(choice, "player2", otherPlayer, otherChoice);
 
         } else if ((player.equals("player2")) && ((choice.equals("o")) || (choice.equals("O")))) {
             details.setPLAYER2(choice.toUpperCase());
             System.out.println("\nplayer2 has chosen =====> " + details.getPLAYER2() + " ....so " + otherPlayer + " can take =====> X");
             String otherChoice = "x";
+            playerChoice1="O";
+            playerChoice2="X";
             game(choice, "player2", otherPlayer, otherChoice);
         }
     }
@@ -71,9 +80,11 @@ public class PlayerMethods {
         }else
             if(player.equals("player1")) {
                  System.out.println("\n====================================================  Hey player1 !!! its you turn ====================================================");
+                 chances(playerChoice1);
                  game(choice, player, otherPlayer, otherChoice);
             } else if (player.equals("player2")) {
                 System.out.println("\n====================================================  Hey player2 !!! its you turn ====================================================");
+                chances(playerChoice2);
                 game(choice, player, otherPlayer, otherChoice);
         }
     }
@@ -164,4 +175,33 @@ public class PlayerMethods {
             System.out.println("you have stopped playing the game... bye bye");
             while(true);}
     }
-   }
+
+    public void chances(String choice){
+        String A=choice;
+        String row1 = (gameBoard[0][0] + gameBoard[0][1] + gameBoard[0][2]);
+        String row2 = (gameBoard[1][0] + gameBoard[1][1] + gameBoard[1][2]);
+        String row3 = (gameBoard[2][0] + gameBoard[2][1] + gameBoard[2][2]);
+        String colum1 = (gameBoard[0][0] + gameBoard[1][0] + gameBoard[2][0]);
+        String colum2 = (gameBoard[0][1] + gameBoard[1][1] + gameBoard[2][1]);
+        String colum3 = (gameBoard[2][0] + gameBoard[2][1] + gameBoard[2][2]);
+        String leftToRightDiagonal=(gameBoard[0][0] + gameBoard[1][1] + gameBoard[2][2]);
+        String rightToLeftDiagonal=(gameBoard[0][2] + gameBoard[1][1] + gameBoard[2][0]);
+
+        if(((row1.equals(A+A+"3"))||(row1.equals("1"+A+A))||(row1.equals(A+"2"+A))))
+            System.out.println("\n\t\t\t"+A+" ===> choose any empty positions among {1,2,3} ");
+        else if(((row2.equals(A+A+"6"))||(row2.equals("4"+A+A))||(row2.equals(A+"5"+A))))
+            System.out.println("\n\t\t\t"+A+" ===> choose any empty positions among {4,5,6} ");
+        else if(((row3.equals(A+A+"9"))||(row3.equals("7"+A+A))||(row3.equals(A+"8"+A))))
+            System.out.println("\n\t\t\t"+A+" ===>  choose any empty positions among {7,8,9} ");
+        else if(((colum1.equals(A+A+"7"))||(colum1.equals("1"+A+A))||(colum1.equals(A+"4"+A))))
+            System.out.println("\n\t\t\t"+A+" ===>  choose any empty positions among {4,1,7} ");
+        else if(((colum2.equals(A+A+"8"))||(colum2.equals("2"+A+A))||(colum2.equals(A+"5"+A))))
+            System.out.println("\n\t\t\t"+A+" ===>  choose any empty positions among {2,5,8} ");
+        else if(((colum3.equals(A+A+"9"))||(colum3.equals("3"+A+A))||(colum3.equals(A+"6"+A))))
+            System.out.println("\n\t\t\t"+A+" ===>  choose any empty positions among {3,6,9}");
+        else if(((leftToRightDiagonal.equals(A+A+"9"))||(colum3.equals("1"+A+A))||(colum3.equals(A+"5"+A))))
+            System.out.println("\n\t\t\t"+A+" ===>  choose any empty positions among {1,5,9} ");
+        else if(((rightToLeftDiagonal.equals(A+A+"7"))||(colum3.equals("3"+A+A))||(colum3.equals(A+"5"+A))))
+            System.out.println("\n\t\t\t"+A+" ===>  choose any empty positions among {3,5,7} ");
+    }
+}
